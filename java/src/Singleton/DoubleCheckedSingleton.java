8d123e25167a58527132c40dd2d365ab87155bc9
zhangxian01 <zhangxian01@ky-tech.com.cn>
2020-09-30 15:54:14 +0800

    单例模式

---
+++ java/src/Singleton/DoubleCheckedSingleton.java
@@ -0,0 +1,26 @@
+package Singleton;
+
+/**
+ * JDK 版本：JDK1.5 起
+ * 是否 Lazy 初始化：是
+ * 是否多线程安全：是
+ * 实现难度：较复杂
+ * 描述：这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+ *   getInstance() 的性能对应用程序很关键。
+ */
+public class DoubleCheckedSingleton {
+    private static DoubleCheckedSingleton instance;
+
+    private DoubleCheckedSingleton(){};
+
+    public static DoubleCheckedSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DoubleCheckedSingleton.class) {
+                if (instance == null) {
+                    instance = new DoubleCheckedSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
---
+++ java/src/Singleton/EagerSingleton.java
@@ -0,0 +1,26 @@
+package Singleton;
+
+/**
+ * 是否 Lazy 初始化：否
+ * 是否多线程安全：是
+ * 实现难度：易
+ * 描述：这种方式比较常用，但容易产生垃圾对象
+ * 优点：没有加锁，执行效率会提高。
+ *       如果初始化耗时长：初始化耗时长时，提前初始化可以提升系统性能；
+ *       如果实例占用资源多，按照 fail-fast 的设计原则（有问题及早暴露），
+ *   如果资源不够，就会在程序启动的时候触发报错（比如 Java 中的 PermGen Space OOM），我们可以立即去修复，
+ *   从而避免程序运行一段时间后，突然因为初始化这个实例占用资源过多，导致系统崩溃，影响系统的可用性。
+ * 缺点：类加载时就初始化，浪费内存。
+ * 原理：它基于 classloader 机制避免了多线程的同步问题，不过，instance 在类装载时就实例化，虽然导致类装载的原因有很多种，
+ *   在单例模式中大多数都是调用 getInstance 方法， 但是也不能确定有其他的方式（或者其他的静态方法）导致类装载，这时候初始化 instance 显然没有达到 lazy loading 的效果。
+ */
+public class EagerSingleton {
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {
+    }
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
