8a06950fa5db52af7dca68d6cf3ed14a79852163
zhangxian01 <zhangxian01@ky-tech.com.cn>
2020-11-05 11:40:52 +0800

    原型模式-原型管理器

---
+++ java/src/creating/prototype/manager/OfficialDocument.java
@@ -0,0 +1,6 @@
+package creating.prototype.manager;
+
+public interface OfficialDocument extends Cloneable{
+    public  OfficialDocument clone();
+    public  void display();
+}
---
+++ java/src/creating/prototype/manager/PrototypeManager.java
@@ -0,0 +1,53 @@
+package creating.prototype.manager;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PrototypeManager {
+
+    //定义一个map，用于存储原型对象
+    private Map<String,OfficialDocument> map= new ConcurrentHashMap<>(2);
+
+    private static PrototypeManager pm =  new PrototypeManager();
+
+    //为map增加公文对象
+    private  PrototypeManager() {
+        map.put("far",new  FAR());
+        map.put("srs",new  SRS());
+    }
+
+    //增加新的公文对象
+
+    public void addOfficialDocument(String  key,OfficialDocument doc) {
+        map.put(key,doc);
+    }
+
+    //通过浅克隆获取新的公文对象
+    public OfficialDocument  getOfficialDocument(String key){
+        return  ((OfficialDocument)map.get(key)).clone();
+    }
+
+    public static PrototypeManager  getPrototypeManager() {
+        return pm;
+    }
+}
+
+class Demo {
+    public  static void main(String args[]) {
+        //获取原型管理器对象
+        PrototypeManager pm =  PrototypeManager.getPrototypeManager();
+        OfficialDocument  doc1,doc2,doc3,doc4;
+        doc1  = pm.getOfficialDocument("far");
+        doc1.display();
+        doc2  = pm.getOfficialDocument("far");
+        doc2.display();
+        System.out.println(doc1  == doc2);
+        doc3  = pm.getOfficialDocument("srs");
+        doc3.display();
+        doc4  = pm.getOfficialDocument("srs");
+        doc4.display();
+        System.out.println(doc3  == doc4);
+    }
+
+}
